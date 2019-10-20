7ff94efc6a12d3c57fee651f2010b8dff30748fa
Enrique Arizón Benito <earizonb@everis.com>
2019-10-20 21:22:31 +0200

    Add constructor and owner to contract

--- contracts/MicroBank.sol
+++ contracts/MicroBank.sol
@@ -1,4 +1,25 @@
 pragma solidity >=0.5.0 <0.6;
 
+    /*
+     * MicroBank allows to fund accounts and transfer
+     * money amongst accounts.
+     * Each account ID corresponds to an Ethereum public address
+     */
 contract MicroBank {
+
+    address public owner;
+
+    // Constructor, can receive one or many variables here; only one allowed
+    constructor ()  
+    public                                                                                                                                
+    {   
+        // msg provides details about the message that's sent to the contract
+        // msg.sender is contract caller (address of contract creator)
+        owner     = msg.sender;
+     // ^^^^^       ^^^^^^^^^^
+     // No this.    All external functions, included constructors, are called through
+     // needed      JSON-RPC sending a signed TX. The public address matching the
+     //             signed can be fetch as msg.sender
+    }
+
 }
